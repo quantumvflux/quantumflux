26dcd01fdcaa6cd997464777fe9613d49d11469d
quantumvflux <marcosabaravalle@gmail.com>
2024-09-30 08:31:43 -0300

    add cringy text

--- app/home/main/Main.tsx
+++ app/home/main/Main.tsx
@@ -1,13 +1,22 @@
-import { Heading } from "@/app/components/Heading"
-import { Section } from "@/app/components/Section"
+import { Heading } from "@/app/components/Heading";
+import { Section } from "@/app/components/Section";
 
 export const Main = () => {
-    return (
-        <div>
-            <Section>
-                <Heading text="about:" />
-                <p className="text-xl">Some boring sh*t right here...</p>
-            </Section>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Section>
+        <Heading text="about:" />
+        <p className="text-xl">
+          Hello, everyone! I'm Marcos, a freelance web developer hailing from
+          Argentina. Specializing in crafting bespoke digital marketing
+          solutions, my focus is on tailoring these to suit your enterprise or
+          personal brand. My creative thinking and problem-solving abilities are
+          fueled by a passion for technology, entrepreneurship, video games,
+          music, and economics. Embracing new ideas and maintaining an open mind
+          allows me to approach projects from various angles, ensuring the
+          delivery of innovative solutions ^^
+        </p>
+      </Section>
+    </div>
+  );
+};
