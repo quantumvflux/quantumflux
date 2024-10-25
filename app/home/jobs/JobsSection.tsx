d1617300f894a09a776118c2d9e2ecc3ccd625dc
quantumvflux <marcosabaravalle@gmail.com>
2024-10-25 15:53:51 -0300

    fix

--- app/home/jobs/JobsSection.tsx
+++ app/home/jobs/JobsSection.tsx
@@ -1,18 +1,23 @@
-import { Heading } from "@/app/components/Heading"
-import { Section } from "@/app/components/Section"
-import { jobz } from "@/app/consts/data"
-import { JobItem } from "./jobItem/JobItem"
-import { IJobz } from "@/app/interfaces/interfaces"
+import { Heading } from "@/app/components/Heading";
+import { Section } from "@/app/components/Section";
+import { jobz } from "@/app/consts/data";
+import { JobItem } from "./jobItem/JobItem";
+import { IJobz } from "@/app/interfaces/interfaces";
 
 export const JobsSection = () => {
-    return (
-        <Section>
-            <Heading text="companies/people i have worked with:" />
-            <div className="flex justify-center flex-wrap gap-4">
-                {jobz.map(({ name, description, image, link }) => (
-                    <JobItem name={name} description={description} image={image} link={link} key={name} />
-                ))}
-            </div>
-        </Section>
-    )
-}
+  return (
+    <Section>
+      <Heading text="companies/people i have worked with:" />
+      <div className="flex justify-center flex-wrap gap-4">
+        {jobz.map(({ name, description, image }) => (
+          <JobItem
+            name={name}
+            description={description}
+            image={image}
+            key={name}
+          />
+        ))}
+      </div>
+    </Section>
+  );
+};
