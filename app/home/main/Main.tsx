e8cd9b0406a6e97e98d4c7c35376a38500ec581b
quantumvflux <marcosabaravalle@gmail.com>
2025-05-07 11:47:41 -0300

    add social buttons to about section

--- app/home/main/Main.tsx
+++ app/home/main/Main.tsx
@@ -6,7 +6,7 @@ export const Main = () => {
     <div>
       <Section>
         <Heading text="about:" />
-        <p className="text-2xl ">
+        <p className="text-2xl mb-6">
           Hi, everyone! I&apos;m Marcos, a software developer from Argentina and
           I create custom digital marketing solutions and software designed to
           meet the unique needs of your business or personal brand.
@@ -16,6 +16,22 @@ export const Main = () => {
           which helps me tackle projects from different angles and deliver
           fresh, innovative solutions.
         </p>
+        <div className="flex gap-4">
+          <a
+            href="/assets/curriculum/Marcos_Baravalle_curriculum.pdf"
+            download
+            className="text-xl border-2 border-gray-500 px-4 py-1 bg-transparent hover:bg-gray-500 hover:text-white transition-colors"
+          >
+            Download CV
+          </a>
+          <a
+            href="https://www.linkedin.com/in/marcos-baravalle/"
+            className="text-xl border-2 border-gray-500 px-4 py-1 bg-transparent hover:bg-gray-500 hover:text-white transition-colors"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </div>
       </Section>
     </div>
   );
